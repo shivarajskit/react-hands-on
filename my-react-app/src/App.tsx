200bb53ef51472f44d9587cc9f0e433f5c37938c
Shivaraj S <shivarajskit@gmail.com>
2025-07-21 15:56:40 +0530

    Adding routes for Home/Counter/Userlist and Page not found

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,3 +1,5 @@
+import {Routes, Route, Link} from 'react-router-dom';
+
 import Counter from './components/Counter';
 import UserList from './components/UserList';
 
@@ -6,9 +8,26 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h3>Hello, React + TypeScript + Vite 🚀</h3>
-      <Counter />
-      <UserList />
+      
+      
+      <nav>
+        <Link to="/">Home</Link> |
+        <Link to="/counter">Counter</Link> |
+        <Link to="/users">Users</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <h3>Hello, React + TypeScript + Vite 🚀</h3>
+            <h2>React Router Example</h2>
+          </>} 
+          />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="*" element={
+          <h3>Page not found</h3>} 
+        />
+      </Routes>
     </div>
 
   )
