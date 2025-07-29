4edeb7d732fcf6fdf29a370e19d6bb94112ce01f
Shivaraj S <shivarajskit@gmail.com>
2025-07-29 10:52:38 +0530

    redux actions and reducers

---
+++ my-react-app/src/store/actions.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+export const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+export const fetchUsers = () => {
+  return async (dispatch) => {
+    dispatch({ type: FETCH_USERS_REQUEST });
+
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+      dispatch({ type: FETCH_USERS_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: FETCH_USERS_FAILURE, payload: error.message });
+    }
+  };
+};
